140eaf3ae130f0e5170c463d9caa7db0f5060ae0
ChemaArmenta26 <159948311+ChemaArmenta26@users.noreply.github.com>
2025-04-01 12:25:08 -0700

    Proyecto mi pokedex, practica 12

---
+++ app/src/main/java/armenta/jose/myapplication/Pokemon.kt
@@ -0,0 +1,7 @@
+package armenta.jose.myapplication
+
+data class Pokemon(
+    val name: String = "",
+    val number: Long = 0,
+    val imageUrl: String = ""
+    )
---
+++ app/src/main/java/armenta/jose/myapplication/PokemonAdapter.kt
@@ -0,0 +1,34 @@
+package armenta.jose.myapplication
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
+class PokemonAdapter(context: Context, private val pokemonList: List<Pokemon>) :
+    ArrayAdapter<Pokemon>(context, 0, pokemonList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_pokemon, parent, false)
+
+        val pokemon = pokemonList[position]
+        val imgPokemon = view.findViewById<ImageView>(R.id.imgPokemon)
+        val txtNumber = view.findViewById<TextView>(R.id.txtPokemonNumber)
+        val txtName = view.findViewById<TextView>(R.id.txtPokemonName)
+
+        txtNumber.text = "No. ${pokemon.number}"
+        txtName.text = pokemon.name
+
+        Glide.with(context)
+            .load(pokemon.imageUrl)
+            .into(imgPokemon)
+
+        return view
+    }
+}
+
+
